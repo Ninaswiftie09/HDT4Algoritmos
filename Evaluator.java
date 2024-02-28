49a144f259afb607d512f903ee897b115dfaae7c
bastmre44 <134813711+bastmre44@users.noreply.github.com>
2024-02-27 21:28:40 -0600

    Update Evaluator.java
    comentario interfaz

--- Evaluator.java
+++ Evaluator.java
@@ -1,3 +1,7 @@
+
+//Interfaz para el evaluator. 
 public interface Evaluator {
+
+    //método para evaluar 1 expresión y devolver su resultado 
     int evaluateExpression(String expression) throws Exception;
 }
