e6a5270f05e05cf007fff95caaa9e083243220c3
bastmre44 <134813711+bastmre44@users.noreply.github.com>
2024-02-27 21:31:56 -0600

    Update EvaluatorFactory.java
    comentarios

--- EvaluatorFactory.java
+++ EvaluatorFactory.java
@@ -1,9 +1,13 @@
+//
+
 public class EvaluatorFactory {
+      //   * Método estático para obtener un evaluador de expresiones matemáticas.
+
     public static Evaluator getEvaluator(String type) {
         if (type.equalsIgnoreCase("infix")) {
-            return new Infix();
+            return new Infix(); //si el tipo es infix se retorna un objeto de la clase Infix
         } else if (type.equalsIgnoreCase("postfix")) {
-            return new Postfix();
+            return new Postfix(); // si el tipo no es valido se retorna null
         }
         return null;
     }
